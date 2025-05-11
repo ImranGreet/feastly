652b12fa20575bec6dd03fb6cefb7d0102469a89
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2025-05-11 22:29:25 +0600

    authentication module is defined

---
+++ src/pages/authentication/views/Registration.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-2xl bg-red-200 text-gray-800">Registration</h1>
+    </div>
+</template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,18 @@ import {
   type RouteRecordRaw,
 } from "vue-router";
 import Authlayout from "../components/layouts/Authlayout.vue";
+import Registration from "../pages/authentication/views/Registration.vue";
 
 const routes: RouteRecordRaw[] = [
     {
         path:'/',
         name:'home',
         component:Authlayout
+    },
+    {
+      path:'/registration',
+      name:'registration',
+      component:Registration
     }
 ];
 
